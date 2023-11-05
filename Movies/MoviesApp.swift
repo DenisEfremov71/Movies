8fb6958acd8e4a82b93525266e901a261b4370f6
Denis Efremov <efremov@novuscom.net>
2023-11-05 05:35:06 -0800

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Denis Efremov on 2023-11-05.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
